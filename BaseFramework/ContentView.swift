ab387d5e39a0d553f05dc6f9b69d37596bfe12bc
cmh <3536535388@qq.com>
2020-03-12 15:26:38 +0800

    create project

---
+++ BaseFramework/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaseFramework
+//
+//  Created by Cmh on 2020/3/12.
+//  Copyright © 2020 Cmh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
